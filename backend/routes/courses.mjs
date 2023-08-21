1ba997579ade54dabe09d513925e063f2dd532e7
bociwess0 <dimitrijeveljkovic2@gmail.com>
2023-08-21 21:14:36 +0200

    New endpoint for counters

--- backend/routes/courses.mjs
+++ backend/routes/courses.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
 import Course from '../models/course.mjs';
 import tokenCheckMiddleware from '../middleware/token-check.mjs';
+import LearningPath from '../models/learning-path.mjs';
+import User from '../models/user.mjs';
 
 const router = express.Router();
 
@@ -47,6 +49,36 @@ router.post('/:courseId/add-comment', tokenCheckMiddleware, (req, res, next) => 
 
         course.save().then(() => {
             res.status(200).json({ comments: course.comments });
+        });
+    });
+});
+
+// Return number of all courses
+router.get('/count', (req, res, next) => {
+    Course.countDocuments().then(courseCount => {
+        LearningPath.countDocuments().then(learningPathCount => {
+            if (!req.query.userId) {
+                return res.status(200).json({
+                    courseCount,
+                    learningPathCount
+                })
+            }
+
+            User.findById(req.query.userId).then(user => {
+                if (!user) {
+                    return res.status(404).json({
+                        message: 'User not found!'
+                    });
+                }
+
+                return res.status(200).json({
+                    courseCount,
+                    learningPathCount,
+                    inProgressCount: user.inProgressCourses.length,
+                    bookmarkCount: user.bookmarkedCourses.length,
+                    completeCount: user.finishedCourses.length
+                });
+            })
         });
     });
 });
