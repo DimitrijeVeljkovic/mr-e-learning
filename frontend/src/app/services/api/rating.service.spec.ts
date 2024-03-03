5e867404b511f37f60fe119ce9e325282e141010
DimitrijeVeljkovic <dimitrijeveljkovic2@gmail.com>
2024-03-03 13:58:27 +0100

    Setup Jest for unit testing

--- frontend/src/app/services/api/rating.service.spec.ts
+++ frontend/src/app/services/api/rating.service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { RatingService } from './rating.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('RatingService', () => {
   let service: RatingService;
@@ -12,5 +13,13 @@ describe('RatingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('api', () => {
+    it('should call add rating api', inject([HttpClient], (http: HttpClient) => {
+      spyOn(http, 'post');
+      service.addRating({ userId: 1, rating: 5 }, 2);
+      expect(http.post).toHaveBeenCalledWith('http://localhost:8080/api/rating?courseId=2', { userId: 1, rating: 5 });
+    }))
   });
 });
